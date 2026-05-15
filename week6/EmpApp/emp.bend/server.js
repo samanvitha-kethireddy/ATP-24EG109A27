8316a65acf859e919046d668ed0a6a074aeead7a
samanvitha-kethireddy <samanvithareddy07@gmail.com>
2026-05-15 15:32:17 +0530

    Modified

--- week6/EmpApp/emp.bend/server.js
+++ week6/EmpApp/emp.bend/server.js
@@ -9,7 +9,7 @@ const app = exp()
 //add cors middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://samanvitha-empapp.vercel.app/"],
+    origin: ["http://localhost:5173", "https://samanvitha-empapp.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
     credentials: true
   }),
